751d2346358929ce6332246a3960b0dd19395a43
UdayPopuri <up6710@srmist.edu.in>
2026-02-20 15:55:05 +0530

    Added UCL

---
+++ step3.java
@@ -0,0 +1,46 @@
+class step3 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("",
+                "  *****  ", // O
+                "  *****  ", // O
+                "  ****** ", // P
+                "  *****  ")); // S
+
+        System.out.println(String.join("",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * "));
+
+        System.out.println(String.join("",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *      "));
+
+        System.out.println(String.join("",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                "  *****  "));
+
+        System.out.println(String.join("",
+                " *     * ",
+                " *     * ",
+                " *       ",
+                "       * "));
+
+        System.out.println(String.join("",
+                " *     * ",
+                " *     * ",
+                " *       ",
+                " *     * "));
+
+        System.out.println(String.join("",
+                "  *****  ",
+                "  *****  ",
+                " *       ",
+                "  *****  "));
+    }
+}
